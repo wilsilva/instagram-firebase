88657869f6cc82650ef28b932e0e03325a7e2f08
Tiago Henrique Freitas Turibio <tiagozhk@gmail.com>
2017-11-09 17:22:47 -0200

    Inserting login instagram image container

--- Instagram/controller/LoginController.swift
+++ Instagram/controller/LoginController.swift
@@ -26,6 +26,13 @@ class LoginController: UIViewController {
         return stackView
     }()
     
+    let bluBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     let emailTextField: UITextField = {
         let textField = UITextField()
         textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedStringKey.foregroundColor : UIColor(white: 0, alpha: 0.2)])
@@ -130,15 +137,18 @@ class LoginController: UIViewController {
         view.backgroundColor = UIColor.white
         view.addSubview(loginStackView)
         view.addSubview(signUpButton)
+        view.addSubview(bluBackground)
         
         navigationController?.isNavigationBarHidden = true
         
         validateLogin()
         
+        bluBackground.anchors(top: view.topAnchor, right: view.rightAnchor, bottom: nil, left: view.leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 0, height: 200)
+        
         loginStackView.insertArrangedSubview(emailTextField, at: 0)
         loginStackView.insertArrangedSubview(passwordTextField, at: 1)
         loginStackView.insertArrangedSubview(loginButton, at: 2)
-        loginStackView.anchors(top: view.topAnchor, right: view.rightAnchor, bottom: nil, left: view.leftAnchor, paddingTop: 20, paddingRight: -20, paddingBottom: 0, paddingLeft: 20, width: 0, height: 150)
+        loginStackView.anchors(top: bluBackground.bottomAnchor, right: view.rightAnchor, bottom: nil, left: view.leftAnchor, paddingTop: 40, paddingRight: -20, paddingBottom: 0, paddingLeft: 20, width: 0, height: 150)
         
         signUpButton.anchors(top: nil, right: nil, bottom: view.bottomAnchor, left: nil, paddingTop: 0, paddingRight: 0, paddingBottom: -10, paddingLeft: 0, width: 0, height: 0)
         signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
