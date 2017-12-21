77e0f21799e7eacab37641cc5924e9f6a8a74f6c
Tiago Henrique Freitas Turibio <tiagozhk@gmail.com>
2017-12-21 14:20:16 -0200

    Postcell implementation

--- Instagram/view/home/PostCell.swift
+++ Instagram/view/home/PostCell.swift
@@ -11,9 +11,10 @@ import UIKit
 
 class PostCell: UICollectionViewCell{
     static var ID = "postCell"
+    var imageCache = [String:UIImage?]()
     var post: Post?{
         didSet{
-            
+            loadPost()
         }
     }
     
@@ -30,6 +31,16 @@ class PostCell: UICollectionViewCell{
         stackView.alignment = .fill
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    let commentsContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .gray
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.distribution = .fill
         return stackView
     }()
     
@@ -56,7 +67,6 @@ class PostCell: UICollectionViewCell{
     let postImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = .green
         return imageView
     }()
     
@@ -67,7 +77,7 @@ class PostCell: UICollectionViewCell{
         return button
     }()
     
-    let commentPost: UIButton = {
+    let postComment: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
@@ -91,12 +101,14 @@ class PostCell: UICollectionViewCell{
     let postCaption: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.s"
         return label
     }()
     
     let postDate: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "5h ago"
         return label
     }()
     
@@ -110,29 +122,67 @@ class PostCell: UICollectionViewCell{
         addSubview(postImage)
         addSubview(footerContainer)
         addSubview(savePost)
+        addSubview(commentsContainer)
         
         headerContainer.addSubview(userProfileImage)
         headerContainer.addSubview(userName)
         headerContainer.addSubview(moreOptions)
-        headerContainer.anchors(top: topAnchor, right: rightAnchor, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 0, height: 56)
+        headerContainer.anchors(top: topAnchor, right: rightAnchor, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: -16, paddingBottom: 0, paddingLeft: 0, width: 0, height: 60)
         
-        userProfileImage.anchors(top: topAnchor, right: nil, bottom: nil, left: leftAnchor, paddingTop: 8, paddingRight: 0, paddingBottom: 0, paddingLeft: 8, width: 40, height: 40)
-        
-        userName.anchors(top: topAnchor, right: nil, bottom: nil, left: userProfileImage.rightAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 8, width: 0, height: 0)
-        userName.centerYAnchor.constraint(equalTo: userProfileImage.centerYAnchor, constant: 0).isActive = true
-        
-        moreOptions.anchors(top: topAnchor, right: rightAnchor, bottom: nil, left: nil, paddingTop: 0, paddingRight: -8, paddingBottom: 0, paddingLeft: 0, width: 0, height: 0)
+        userProfileImage.anchors(top: nil, right: nil, bottom: nil, left: headerContainer.leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 8, width: 35, height: 35)
+        userName.anchors(top: nil, right: nil, bottom: nil, left: userProfileImage.rightAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 8, width: 0, height: 0)
+        moreOptions.anchors(top: headerContainer.topAnchor, right: headerContainer.rightAnchor, bottom: nil, left: nil, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 0, height: 0)
+        userName.centerYAnchor.constraint(equalTo: headerContainer.centerYAnchor, constant: 0).isActive = true
+        userProfileImage.centerYAnchor.constraint(equalTo: headerContainer.centerYAnchor, constant: 0).isActive = true
         moreOptions.centerYAnchor.constraint(equalTo: userProfileImage.centerYAnchor, constant: 0).isActive = true
         
-        postImage.anchors(top: headerContainer.bottomAnchor, right: rightAnchor, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 0, height: 100)
+        postImage.anchors(top: headerContainer.bottomAnchor, right: rightAnchor, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 0, height: 350)
         
         footerContainer.anchors(top: postImage.bottomAnchor, right: nil, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0, width: 120, height: 56)
         footerContainer.insertArrangedSubview(likePost, at: 0)
-        footerContainer.insertArrangedSubview(commentPost, at: 1)
+        footerContainer.insertArrangedSubview(postComment, at: 1)
         footerContainer.insertArrangedSubview(sharePost, at: 2)
         
         savePost.anchors(top: postImage.bottomAnchor, right: rightAnchor, bottom: nil, left: nil, paddingTop: 0, paddingRight: -8, paddingBottom: 0, paddingLeft: 0, width: 0, height: 0)
         savePost.centerYAnchor.constraint(equalTo: footerContainer.centerYAnchor).isActive = true
+        
+        commentsContainer.anchors(top: footerContainer.bottomAnchor, right: rightAnchor, bottom: nil, left: leftAnchor, paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 8, width: 0, height: 0)
+        commentsContainer.insertArrangedSubview(postCaption, at: 0)
+        commentsContainer.insertArrangedSubview(postDate, at: 1)
+        
+    }
+    
+    func loadPost(){
+        if let post = post{
+            if let url = post.url{
+                if let cachedImage = imageCache[url.absoluteString]{
+                    self.postImage.image = cachedImage
+                    return
+                }
+                
+                loadImageWith(url: url, completion: { [weak self] (data) in
+                    self?.imageCache[url.absoluteString] = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        self?.postImage.image = UIImage(data: data)
+                    }
+                })
+            }
+        }
+    }
+    
+    fileprivate func loadImageWith(url imageUrl: URL, completion: ((_ data:Data)->Void)?){
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: imageUrl) { (data, response, error) in
+            if let error = error{
+                print(error)
+                return
+            }
+            if let data = data{
+                if let completion = completion{
+                    completion(data)
+                }
+            }
+            }.resume()
     }
     
     required init?(coder aDecoder: NSCoder) {
